984d3e01bb94541b86891ee9343255a3a3d8f832
Joseph Butler <joseph.butler@intel.com>
2016-11-15 09:05:47 -0700

    Update blink.py

--- blink-led-py/blink.py
+++ blink-led-py/blink.py
@@ -19,6 +19,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+##requires installation of mraa: https://github.com/intel-iot-devkit/mraa#installing-on-ubuntu 
+
 import mraa
 import time
 
